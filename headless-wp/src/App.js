510f3e23fa0673d6491ea840b7fa4f06e398885d
Ash Whiting <awhiting@thisispegasus.co.uk>
2017-07-06 16:25:53 +0100

    amend

--- headless-wp/src/App.js
+++ headless-wp/src/App.js
@@ -19,12 +19,12 @@ componentDidMount() {
 render() {
     let movies = this.state.movies.map((movie, index) => {
       return <div key={index}>
-      <img src={movie._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url} alt="" />
-      <p><strong>Title:</strong> {movie.title.rendered}</p>
-      <p><strong>Release Year:</strong> {movie.acf.release_year}</p>
-      <p><strong>Rating:</strong> {movie.acf.rating}</p>
-      <div><strong>Description:</strong><div dangerouslySetInnerHTML={ {__html: movie.acf.description} } /></div>
-      </div>
+              <img src={movie._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url} alt="" />
+              <p><strong>Title:</strong> {movie.title.rendered}</p>
+              <p><strong>Release Year:</strong> {movie.acf.release_year}</p>
+              <p><strong>Rating:</strong> {movie.acf.rating}</p>
+              <div><strong>Description:</strong><div dangerouslySetInnerHTML={ {__html: movie.acf.description} } /></div>
+            </div>
     });
 return (
       <div>
